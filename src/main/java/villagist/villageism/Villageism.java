9110d4e98d8069c9c7636153ef1ad60b33af1bfe
green_orange <yangjz1125@outlook.com>
2023-08-01 10:40:43 +0800

    initial commit

---
+++ src/main/java/villagist/villageism/Villageism.java
@@ -0,0 +1,13 @@
+package villagist.villageism;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Villageism implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/villagist/villageism/client/VillageismClient.java
@@ -0,0 +1,13 @@
+package villagist.villageism.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VillageismClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
