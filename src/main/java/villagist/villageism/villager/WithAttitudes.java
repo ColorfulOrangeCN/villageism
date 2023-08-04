4e5eb56b5fa80afd55c9d59e65ca13bd551d72a7
green_orange <yangjz1125@outlook.com>
2023-08-04 18:04:38 +0800

    Do some refactors.

--- src/main/java/villagist/villageism/villager/WithAttitudes.java
+++ src/main/java/villagist/villageism/villager/WithAttitudes.java
@@ -6,16 +6,16 @@ import net.minecraft.nbt.NbtCompound;
 import org.jetbrains.annotations.NotNull;
 
 public interface WithAttitudes {
-    public static String ATTITUDES_KEY = "AttitudesList";
-    public Attitude[] getAttitudes();
-    public void setAttitudes(Attitude[] attitudes);
-    public default void writeToNbt(@NotNull NbtCompound nbt) {
+    String ATTITUDES_KEY = "AttitudesList";
+    Attitude[] getAttitudes();
+    void setAttitudes(Attitude[] attitudes);
+    default void writeToNbt(@NotNull NbtCompound nbt) {
         nbt.putString(ATTITUDES_KEY, new Gson().toJson(getAttitudes()));
     }
-    public default void loadFromNbt(@NotNull NbtCompound nbt) {
+    default void loadFromNbt(@NotNull NbtCompound nbt) {
         setAttitudes(new Gson().fromJson(nbt.getString(ATTITUDES_KEY), Attitude[].class));
     }
-    public default int getAttitudeTo(@NotNull LivingEntity entity) {
+    default int getAttitudeTo(@NotNull LivingEntity entity) {
         for (Attitude attitude : getAttitudes()) {
             if (attitude.uuid.equals(entity.getUuid())) {
                 return attitude.attitude;
